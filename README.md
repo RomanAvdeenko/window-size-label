7905f0d7a943c699e66ea9c7ca6b2af4c95de588
RomanAvdeenko <yes@hl.ua>
2020-10-01 16:19:12 +0300

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# window-size-label
+
+> Label that contain Window size
+
+[![NPM](https://img.shields.io/npm/v/window-size-label.svg)](https://www.npmjs.com/package/window-size-label) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Install
+
+```bash
+npm install --save window-size-label
+```
+
+## Usage
+
+```jsx
+import React, { Component } from 'react'
+
+import MyComponent from 'window-size-label'
+import 'window-size-label/dist/index.css'
+
+class Example extends Component {
+  render() {
+    return <MyComponent />
+  }
+}
+```
+
+## License
+
+MIT © [RomanAvdeenko](https://github.com/RomanAvdeenko)
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+export default class WindowSizeLabel extends React.Component {
+  state = {
+    windowSize: { width: window.innerWidth, height: window.innerHeight }
+  }
+  UpdateWindowSizeState = () => {
+    this.setState(
+      {
+        windowSize: { width: window.innerWidth, height: window.innerHeight }
+      })
+  }
+  componentDidMount() {
+    window.addEventListener('resize', this.UpdateWindowSizeState)
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.UpdateWindowSizeState)
+  }
+  render() {
+    return (
+      <span className={styles.test}> {this.state.windowSize.width}x{this.state.windowSize.height} </span>
+    )
+  }
+}
